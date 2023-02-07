fe223eff487f887cdbb017f01e857393be2b7d68
Blessing <blessing.raolane@sovtech.com>
2023-02-07 19:53:03 +0200

    draft 0

--- src/app/app/home/main/main/carousel/carousel/carousel.component.ts
+++ src/app/app/home/main/main/carousel/carousel/carousel.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent implements OnInit {
+  projects: { name: string; description: string; url: string }[] = [
+    {
+      name: 'Project 1',
+      description: 'THis is the description',
+      url: 'https://i.pinimg.com/originals/2e/65/75/2e6575d405dea87b573cb0341deacca9.jpg',
+    },
+    {
+      name: 'Project 2',
+      description: 'THis is the description',
+      url: 'https://assets.airtrfx.com/media-em/af/cities/Niza-NCE.jpg?width=500&quality=80&fit=crop&format=auto&opt=true',
+    },
+    {
+      name: 'Project 3',
+      description: 'THis is the description',
+      url: 'https://st2.depositphotos.com/1006220/5360/i/450/depositphotos_53607011-stock-photo-mobile-people-and-stable-buildings.jpg',
+    },
+    {
+      name: 'Project 4',
+      description: 'THis is the description',
+      url: 'https://i.pinimg.com/originals/2e/65/75/2e6575d405dea87b573cb0341deacca9.jpg',
+    },
+    {
+      name: 'Project 5',
+      description: 'THis is the description',
+      url: 'https://i.pinimg.com/originals/2e/65/75/2e6575d405dea87b573cb0341deacca9.jpg',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
